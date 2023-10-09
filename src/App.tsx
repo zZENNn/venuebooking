795f03ec71ff403faed632d8e831f55101178e87
zzzennn <arseniy.nefyodov@gmail.com>
2023-10-09 20:53:15 +0700

    fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
-import WebApp from '@twa-dev/sdk'
+//import WebApp from '@twa-dev/sdk'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
